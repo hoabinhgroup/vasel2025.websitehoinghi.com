4bde522016882a319cc39336adaf5d48e7b6bed5
hoabinhgroup <louis.standbyme@gmail.com>
2025-05-06 14:04:31 +0700

    fix: update course topics in courses function

--- Themes/vasel2025/functions/functions.php
+++ Themes/vasel2025/functions/functions.php
@@ -224,9 +224,9 @@ if (!function_exists("courses")) {
     function courses()
     {
         return [
-            'Chuyên đề Tiêu hóa: Phẫu thuật điều trị thoát vị thành bụng',
-            'Chuyên đề Tiết niệu: Bóc nhân phì đại tuyến tiền liệt nội soi qua đường niệu đạo',
-            'Chuyên đề Chấn thương: Phẫu thuật nội soi khớp vai điều trị rách chóp xoay'
+            'Tán sỏi thận qua da',
+            'Phẫu thuật điều trị thoát vị bẹn',
+            'Phẫu thuật nội soi khớp gối'
         ];
     }
 }
