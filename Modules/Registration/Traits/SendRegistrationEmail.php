3016076c84997b5ed47630ef137e70f84d2e557b
hoabinhgroup <louis.standbyme@gmail.com>
2025-09-22 14:35:30 +0700

    refactor: clean up formatting and improve code readability in SendRegistrationEmail trait

--- Modules/Registration/Traits/SendRegistrationEmail.php
+++ Modules/Registration/Traits/SendRegistrationEmail.php
@@ -1,35 +1,37 @@
 <?php
+
 namespace Modules\Registration\Traits;
 
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 
-trait SendRegistrationEmail {
+trait SendRegistrationEmail
+{
 
 
     protected function sending($data, $template)
-       {
-         
-         $smtp = config('registration.email');
-         
-          // send email api
-          $postData = array(
+    {
+
+        $smtp = config('registration.email');
+
+        // send email api
+        $postData = array(
             'sending_server' => $smtp['sending_server'],
-            'email' => $data['to'] ?? $data['email'], 
+            'email' => $data['to'] ?? $data['email'],
             'from_email' => $smtp['from_email'],
-            'from_name' => $smtp['from_name'],
+            'from_name' => config('registration.email.from_name'),
             'subject' => $data['subject'] ?? $smtp['subject'],
             'reply_to' => $smtp['reply_to'],
             'addCC' => $smtp['cc'],
             'template' => $template
-          );
-         
-          $this->sendEmailViaApi(config('registration.email.endpoint'), $postData, 'POST');
-          return true;
-       }
+        );
 
-   protected function sendEmailViaApi($url, $data = [], $method = 'GET')
+        $this->sendEmailViaApi(config('registration.email.endpoint'), $postData, 'POST');
+        return true;
+    }
+
+    protected function sendEmailViaApi($url, $data = [], $method = 'GET')
     {
         try {
             if ($method === 'POST' || $method === 'PUT' || $method === 'PATCH') {
@@ -39,7 +41,7 @@ trait SendRegistrationEmail {
             } elseif ($method === 'GET') {
                 $response = Http::get($url, $data);
             }
-    
+
             // Trả về kết quả dưới dạng mảng
             return $response->json();
         } catch (\Exception $e) {
@@ -47,16 +49,16 @@ trait SendRegistrationEmail {
             return ['error' => $e->getMessage()];
         }
     }
-    
+
     public function parseEmailContent($content, $data)
     {
         $parsed = preg_replace_callback('/{{$(.*?)}}/', function ($matches) use ($data) {
             list($param, $index) = $matches;
-            if( isset($data[$index]) ) {
+            if (isset($data[$index])) {
                 return $data[$index];
             }
         }, $content);
-    
+
         return $parsed;
     }
 }
