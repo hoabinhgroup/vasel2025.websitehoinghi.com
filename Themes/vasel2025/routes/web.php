b486fade80422ae9574195a233dc8eae3fbcc288
hoabinhgroup <louis.standbyme@gmail.com>
2025-11-12 11:33:28 +0700

    update chương trình

--- Themes/vasel2025/routes/web.php
+++ Themes/vasel2025/routes/web.php
@@ -84,7 +84,7 @@ Route::group(
 
 				Route::get("registration", "Vasel2025Controller@registration");
 				Route::get("program", function () {
-					return view('vasel2025::program');
+					return redirect('https://cdn.websitehoinghi.com/vasel2025/VASEL%202025_Ver6.2.pdf');
 				});
 				Route::post("get-fee-total", "Vasel2025Controller@getFeeTotal");
 			}
